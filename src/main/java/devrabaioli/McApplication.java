c3718708a6d7ba3be103cbe2c579ff285ed28833
devrabaioli <devrabaioli@gmail.com>
2022-12-12 11:41:13 +0000

    Security update

--- src/main/java/devrabaioli/McApplication.java
+++ src/main/java/devrabaioli/McApplication.java
@@ -1,7 +1,17 @@
 package devrabaioli;
 
 import org.springframework.boot.SpringApplication;
+import java.util.Arrays;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 public class McApplication {
@@ -10,4 +20,29 @@ public class McApplication {
 		SpringApplication.run(McApplication.class, args);
 	}
 
+	@Configuration
+	@EnableWebSecurity
+	public class SecurityConfig {
+
+		@Bean
+		public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+			http.headers().frameOptions().disable();
+			http.cors().and().csrf().disable();
+			http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+			http.authorizeHttpRequests((auth) -> auth.anyRequest().permitAll());
+
+			return http.build();
+		}
+
+		@Bean
+		CorsConfigurationSource corsConfigurationSource() {
+			CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+			configuration.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "OPTIONS"));
+			final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+			source.registerCorsConfiguration("/**", configuration);
+			return source;
+		}
+
+	}
 }

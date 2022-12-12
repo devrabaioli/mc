7225b33415029790c0d9240ad5fca3b858c3c035
devrabaioli <devrabaioli@gmail.com>
2022-12-12 12:06:34 +0000

    CategoriaService CategoriaRepository

--- src/main/java/devrabaioli/resources/CategoriaResource.java
+++ src/main/java/devrabaioli/resources/CategoriaResource.java
@@ -1,16 +1,26 @@
 package devrabaioli.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import devrabaioli.domain.Categoria;
+import devrabaioli.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method = RequestMethod.GET)
-	public String list() {
-		return "Rest Ok";
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> list(@PathVariable Integer id) {
+		Categoria obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }

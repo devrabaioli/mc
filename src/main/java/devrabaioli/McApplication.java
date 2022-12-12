a0bcfa0ffe550aa1ca1ebe532e3ac440ec1a4ebd
devrabaioli <devrabaioli@gmail.com>
2022-12-12 13:42:36 +0000

    @ManyToMany Produto and Categoria

--- src/main/java/devrabaioli/McApplication.java
+++ src/main/java/devrabaioli/McApplication.java
@@ -16,13 +16,18 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import devrabaioli.domain.Categoria;
+import devrabaioli.domain.Produto;
 import devrabaioli.repositories.CategoriaRepository;
+import devrabaioli.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class McApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(McApplication.class, args);
@@ -32,9 +37,21 @@ public class McApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "Mouse", 300.00);
+		Produto p2 = new Produto(null, "Impressora", 500.00);
+		Produto p3 = new Produto(null, "Computador", 1200.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 	
 
--- src/main/java/devrabaioli/domain/Categoria.java
+++ src/main/java/devrabaioli/domain/Categoria.java
@@ -1,12 +1,15 @@
 package devrabaioli.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 
 
@@ -20,6 +23,9 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {}
 
@@ -44,6 +50,15 @@ public class Categoria implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	
 
 	@Override
 	public int hashCode() {
@@ -61,6 +76,7 @@ public class Categoria implements Serializable {
 		Categoria other = (Categoria) obj;
 		return Objects.equals(id, other.id);
 	}
+
 	
 	
 

97064333199b6a17ec56845c2f28af5cab473eb5
Thiago Rabaioli <117931378+devrabaioli@users.noreply.github.com>
2022-12-28 14:20:24 +0000

    ItemPedido e ItemPedidoPK

--- src/main/java/devrabaioli/McApplication.java
+++ src/main/java/devrabaioli/McApplication.java
@@ -22,6 +22,7 @@ import devrabaioli.domain.Cidade;
 import devrabaioli.domain.Cliente;
 import devrabaioli.domain.Endereco;
 import devrabaioli.domain.Estado;
+import devrabaioli.domain.ItemPedido;
 import devrabaioli.domain.Pagamento;
 import devrabaioli.domain.PagamentoComBoleto;
 import devrabaioli.domain.PagamentoComCartao;
@@ -34,6 +35,7 @@ import devrabaioli.repositories.CidadeRepository;
 import devrabaioli.repositories.ClienteRepository;
 import devrabaioli.repositories.EnderecoRepository;
 import devrabaioli.repositories.EstadoRepository;
+import devrabaioli.repositories.ItemPedidoRepository;
 import devrabaioli.repositories.PagamentoRepository;
 import devrabaioli.repositories.PedidoRepository;
 import devrabaioli.repositories.ProdutoRepository;
@@ -63,6 +65,9 @@ public class McApplication implements CommandLineRunner{
 	
 	@Autowired
 	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(McApplication.class, args);
@@ -118,7 +123,17 @@ public class McApplication implements CommandLineRunner{
 		
 		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
 		
-		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
@@ -128,6 +143,7 @@ public class McApplication implements CommandLineRunner{
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 	
 
--- src/main/java/devrabaioli/domain/Pedido.java
+++ src/main/java/devrabaioli/domain/Pedido.java
@@ -2,7 +2,9 @@ package devrabaioli.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -11,6 +13,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 
@@ -36,6 +39,10 @@ public class Pedido implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
+	
+	
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
 	
 	public Pedido() {}
 
@@ -86,6 +93,15 @@ public class Pedido implements Serializable {
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+	
 
 	@Override
 	public int hashCode() {
@@ -103,6 +119,7 @@ public class Pedido implements Serializable {
 		Pedido other = (Pedido) obj;
 		return Objects.equals(id, other.id);
 	}
+
 	
 	
 
